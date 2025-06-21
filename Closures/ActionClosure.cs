41e1b7367b8173e5d59338ca52aeaf7487680308
Hissal <hissalofficial@gmail.com>
2025-06-21 16:26:42 +0300

    Updated Action closures to be of different types when associated with ref context

---
+++ Closures/ActionClosure.cs
@@ -0,0 +1,142 @@
+﻿namespace Closures;
+
+/// <summary>
+/// Encapsulates an action delegate and context for invocation.
+/// </summary>
+public struct ActionClosure<TContext> {
+    event Action<TContext> Action;
+    public readonly TContext Context;
+
+    public ActionClosure(TContext context, Action<TContext> action) {
+        Context = context;
+        Action = action ?? throw new ArgumentNullException(nameof(action), "Action cannot be null.");
+    }
+    
+    public void AddAction(Action<TContext> action) => Action += action;
+    public void RemoveAction(Action<TContext> action) => Action -= action;
+
+    public void Invoke() => Action.Invoke(Context);
+}
+
+/// <summary>
+/// Encapsulates an action delegate with ref context for invocation.
+/// </summary>
+public struct ActionClosureRef<TContext> {
+    event RefAction<TContext> RefAction;
+    public TContext Context { get; private set; }
+    
+    readonly RefContextBehaviour refContextBehaviour;
+    
+    public ActionClosureRef(TContext context, RefAction<TContext> action, RefContextBehaviour refContextBehaviour = RefContextBehaviour.UpdateStoredContext) {
+        Context = context;
+        RefAction = action ?? throw new ArgumentNullException(nameof(action), "Action cannot be null.");
+        this.refContextBehaviour = refContextBehaviour;
+    }
+    
+    public void AddAction(RefAction<TContext> action) => RefAction += action;
+    public void RemoveAction(RefAction<TContext> action) => RefAction -= action;
+
+    public void Invoke() {
+        var ctx = Context;
+        RefAction.Invoke(ref ctx);
+        
+        if (refContextBehaviour is RefContextBehaviour.KeepStoredContext)
+            return;
+        
+        Context = ctx;
+    }
+}
+
+/// <summary>
+/// Encapsulates an action delegate with an argument and context for invocation.
+/// </summary>
+public struct ActionClosure<TContext, TArg> {
+    event Action<TContext, TArg> Action;
+    public readonly TContext Context;
+
+    public ActionClosure(TContext context, Action<TContext, TArg> action) {
+        Context = context;
+        Action = action ?? throw new ArgumentNullException(nameof(action), "Action cannot be null.");
+    }
+    
+    public void AddAction(Action<TContext, TArg> action) => Action += action;
+    public void RemoveAction(Action<TContext, TArg> action) => Action -= action;
+
+    public void Invoke(TArg arg) => Action.Invoke(Context, arg);
+}
+
+/// <summary>
+/// Encapsulates an action delegate with an argument and ref context for invocation.
+/// </summary>
+public struct ActionClosureRef<TContext, TArg> {
+    event ActionWithRefContext<TContext, TArg> ActionRefCtx;
+    public TContext Context { get; private set; }
+    
+    readonly RefContextBehaviour refContextBehaviour;
+    
+    public ActionClosureRef(TContext context, ActionWithRefContext<TContext, TArg> action, RefContextBehaviour refContextBehaviour = RefContextBehaviour.UpdateStoredContext) {
+        Context = context;
+        ActionRefCtx = action ?? throw new ArgumentNullException(nameof(action), "Action cannot be null.");
+        this.refContextBehaviour = refContextBehaviour;
+    }
+    
+    public void AddAction(ActionWithRefContext<TContext, TArg> action) => ActionRefCtx += action;
+    public void RemoveAction(ActionWithRefContext<TContext, TArg> action) => ActionRefCtx -= action;
+
+    public void Invoke(TArg arg) {
+        var ctx = Context;
+        ActionRefCtx.Invoke(ref ctx, arg);
+        
+        if (refContextBehaviour is RefContextBehaviour.KeepStoredContext)
+            return;
+        
+        Context = ctx;
+    }
+}
+
+/// <summary>
+/// Encapsulates an action delegate with context and a ref argument for invocation.
+/// </summary>
+public struct RefActionClosure<TContext, TArg> {
+    event RefActionWithNormalContext<TContext, TArg> RefActionNormalCtx;
+    public readonly TContext Context;
+    
+    public RefActionClosure(TContext context, RefActionWithNormalContext<TContext, TArg> action) {
+        Context = context;
+        RefActionNormalCtx = action ?? throw new ArgumentNullException(nameof(action), "Action cannot be null.");
+    }
+    
+    public void AddAction(RefActionWithNormalContext<TContext, TArg> action) => RefActionNormalCtx += action;
+    public void RemoveAction(RefActionWithNormalContext<TContext, TArg> action) => RefActionNormalCtx -= action;
+    
+    public void Invoke(ref TArg arg) => RefActionNormalCtx.Invoke(Context, ref arg);
+}
+
+/// <summary>
+/// Encapsulates an action delegate with ref context and a ref argument for invocation.
+/// </summary>
+public struct RefActionClosureRef<TContext, TArg> {
+    event RefAction<TContext, TArg> RefAction;
+    public TContext Context { get; private set; }
+    
+    readonly RefContextBehaviour refContextBehaviour;
+    
+    public RefActionClosureRef(TContext context, RefAction<TContext, TArg> action, RefContextBehaviour refContextBehaviour = RefContextBehaviour.UpdateStoredContext) {
+        Context = context;
+        RefAction = action ?? throw new ArgumentNullException(nameof(action), "Action cannot be null.");
+        this.refContextBehaviour = refContextBehaviour;
+    }
+    
+    public void AddAction(RefAction<TContext, TArg> action) => RefAction += action;
+    public void RemoveAction(RefAction<TContext, TArg> action) => RefAction -= action;
+
+    public void Invoke(ref TArg arg) {
+        var ctx = Context;
+        RefAction.Invoke(ref ctx, ref arg);
+        
+        if (refContextBehaviour is RefContextBehaviour.KeepStoredContext)
+            return;
+        
+        Context = ctx;
+    }
+}
--- Closures/Closure.cs
+++ Closures/Closure.cs
@@ -5,73 +5,61 @@ public enum ClosureContextType {
     Ref
 }
 
+public enum RefContextBehaviour {
+    UpdateStoredContext,
+    KeepStoredContext
+}
+
 /// <summary>
 /// Provides factory methods for creating closure structs that encapsulate context and delegate logic.
 /// </summary>
 public static class Closure {
-    /// <summary>
-    /// Creates an <see cref="ActionClosure{TContext}"/> with the specified context and action.
-    /// </summary>
+    /// <summary> Creates an <see cref="ActionClosure{TContext}"/> with the specified context and action. </summary>
     public static ActionClosure<TContext> CreateAction<TContext>(TContext context, Action<TContext> action) =>
-        ActionClosure<TContext>.Create(context, action);
+        new ActionClosure<TContext>(context, action);
 
-    /// <summary>
-    /// Creates an <see cref="ActionClosure{TContext}"/> with the specified ref context and action.
-    /// </summary>
-    public static ActionClosure<TContext> CreateAction<TContext>(TContext context, RefAction<TContext> action) =>
-        ActionClosure<TContext>.Create(context, action);
+    /// <summary> Creates an <see cref="ActionClosureRef{TContext}"/> with the specified ref context and action. </summary>
+    public static ActionClosureRef<TContext> CreateAction<TContext>(TContext context, RefAction<TContext> action) =>
+        new ActionClosureRef<TContext>(context, action);
     
-    /// <summary>
-    /// Creates an <see cref="ActionClosure{TContext, TArg}"/> with the specified context and action.
-    /// </summary>
+    /// <summary> Creates an <see cref="ActionClosure{TContext, TArg}"/> with the specified context and action. </summary>
     public static ActionClosure<TContext, TArg> CreateAction<TContext, TArg>(TContext context, Action<TContext, TArg> action) =>
-        ActionClosure<TContext, TArg>.Create(context, action);
+        new ActionClosure<TContext, TArg>(context, action);
     
-    /// <summary>
-    /// Creates an <see cref="ActionClosure{TContext, TArg}"/> with the specified ref context and action.
-    /// </summary>
-    public static ActionClosure<TContext, TArg> CreateAction<TContext, TArg>(TContext context, ActionWithRefContext<TContext, TArg> action) =>
-        ActionClosure<TContext, TArg>.Create(context, action);
+    /// <summary> Creates an <see cref="ActionClosureRef{TContext, TArg}"/> with the specified ref context and action. </summary>
+    public static ActionClosureRef<TContext, TArg> CreateAction<TContext, TArg>(TContext context, ActionWithRefContext<TContext, TArg> action) =>
+        new ActionClosureRef<TContext, TArg>(context, action);
     
-    /// <summary>
-    /// Creates a <see cref="RefActionClosure{TContext, TArg}"/> with the specified context and action.
-    /// </summary>
+    /// <summary> Creates a <see cref="RefActionClosure{TContext, TArg}"/> with the specified context and action. </summary>
     public static RefActionClosure<TContext, TArg> CreateRefAction<TContext, TArg>(TContext context, RefActionWithNormalContext<TContext, TArg> action) =>
-        RefActionClosure<TContext, TArg>.Create(context, action);
-    /// <summary>
-    /// Creates a <see cref="RefActionClosure{TContext, TArg}"/> with the specified ref context and action.
-    /// </summary>
-    public static RefActionClosure<TContext, TArg> CreateRefAction<TContext, TArg>(TContext context, RefAction<TContext, TArg> action) =>
-        RefActionClosure<TContext, TArg>.Create(context, action);
+        new RefActionClosure<TContext, TArg>(context, action);
+    
+    /// <summary> Creates a <see cref="RefActionClosureRef{TContext, TArg}"/> with the specified ref context and action. </summary>
+    public static RefActionClosureRef<TContext, TArg> CreateRefAction<TContext, TArg>(TContext context, RefAction<TContext, TArg> action) =>
+        new RefActionClosureRef<TContext, TArg>(context, action);
 
     
-    /// <summary>
-    /// Creates a <see cref="FuncClosure{TContext, TResult}"/> with the specified context and function.
-    /// </summary>
+    /// <summary> Creates a <see cref="FuncClosure{TContext, TResult}"/> with the specified context and function. </summary>
     public static FuncClosure<TContext, TResult> CreateFunc<TContext, TResult>(TContext context, Func<TContext, TResult> func) =>
         FuncClosure<TContext, TResult>.Create(context, func);
 
+    /// <summary> Creates a <see cref="FuncClosure{TContext, TResult}"/> with the specified ref context and function. </summary>
     public static FuncClosure<TContext, TResult> CreateFunc<TContext, TResult>(TContext context, RefFunc<TContext, TResult> func) =>
         FuncClosure<TContext, TResult>.Create(context, func);
     
-    /// <summary>
-    /// Creates a <see cref="FuncClosure{TContext, TArg, TResult}"/> with the specified context and function.
-    /// </summary>
+    /// <summary> Creates a <see cref="FuncClosure{TContext, TArg, TResult}"/> with the specified context and function. </summary>
     public static FuncClosure<TContext, TArg, TResult> CreateFunc<TContext, TArg, TResult>(TContext context, Func<TContext, TArg, TResult> func) =>
         FuncClosure<TContext, TArg, TResult>.Create(context, func);
     
-    /// <summary>
-    /// Creates a <see cref="FuncClosure{TContext, TArg, TResult}"/> with the specified context and function.
-    /// </summary>
+    /// <summary> Creates a <see cref="FuncClosure{TContext, TArg, TResult}"/> with the specified ref context and function. </summary>
     public static FuncClosure<TContext, TArg, TResult> CreateFunc<TContext, TArg, TResult>(TContext context, FuncWithRefContext<TContext, TArg, TResult> func) =>
         FuncClosure<TContext, TArg, TResult>.Create(context, func);
 
-    /// <summary>
-    /// Creates a <see cref="RefFuncClosure{TContext, TArg, TResult}"/> with the specified context and function.
-    /// </summary>
+    /// <summary> Creates a <see cref="RefFuncClosure{TContext, TArg, TResult}"/> with the specified context and function. </summary>
     public static RefFuncClosure<TContext, TArg, TResult> CreateRefFunc<TContext, TArg, TResult>(TContext context, RefFuncWithNormalContext<TContext, TArg, TResult> func) =>
         RefFuncClosure<TContext, TArg, TResult>.Create(context, func);
     
+    /// <summary> Creates a <see cref="RefFuncClosure{TContext, TArg, TResult}"/> with the specified ref context and function. </summary>
     public static RefFuncClosure<TContext, TArg, TResult> CreateRefFunc<TContext, TArg, TResult>(TContext context, RefFunc<TContext, TArg, TResult> func) =>
         RefFuncClosure<TContext, TArg, TResult>.Create(context, func);
 }
